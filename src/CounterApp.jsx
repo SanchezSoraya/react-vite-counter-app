7b76200af26dce1e964b45f81f5cb3e0c8bfabe9
Soraya Sanchez <sorayasanchez0503@gmail.com>
2023-02-17 16:12:32 -0500

    Prueba basica del CounterApp

--- src/CounterApp.jsx
+++ src/CounterApp.jsx
@@ -24,7 +24,7 @@ export const CounterApp = ( { value } ) =>{
 
      <button onClick={ handLeAdd }> +1 </button>
      <button onClick={ handleSubstract }> -1 </button>
-     <button onClick={ handleReset }> Reset </button>
+     <button aria-label="btn-reset" onClick={ handleReset }> Reset </button>
    </>
   ); 
   
